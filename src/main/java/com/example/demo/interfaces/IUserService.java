fa2c27c5b414d8e0bdec41f8d7e0e47cfb6bdcc6
Kevin Zhu <76707463+zhukv98@users.noreply.github.com>
2025-06-26 18:50:29 -0400

    Delete src directory

--- src/main/java/com/example/demo/interfaces/IUserService.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.interfaces;
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface IUserService extends UserDetailsService {
-
-}
--- src/main/java/com/example/demo/repository/AuthorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.example.demo.entity.Author;
-
-@Repository
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-}
